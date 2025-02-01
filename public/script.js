351723de3f12bbf2117905afa0cd07c342437542
Marko Ivanovic <markonis007006@gmail.com>
2025-02-01 17:33:59 +0100

    Dodan border i ring

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 $(function() {
   var plavi = $(".plavi");
   var crveni = $(".crveni");
+  var ring = $(".ring");
   var move = 20;
   var plavihp = 10;
   var crvenihp = 10;
@@ -14,6 +15,23 @@ $(function() {
 
     var currentUp = parseInt($(plavi).css('top')) || 0; // Trenutna vrednost 'top'
     var currentUpr = parseInt($(crveni).css('top')) || 0; // Trenutna vrednost 'top'
+
+    var ringOffset = ring.offset();
+    var ringWidth = ring.width();
+    var ringHeight = ring.height();
+
+    var plaviOffset = plavi.offset();
+    var crveniOffset = crveni.offset();
+
+    var isPlaviInRing = plaviOffset.left >= ringOffset.left &&
+                        plaviOffset.top >= ringOffset.top &&
+                        plaviOffset.left + plavi.width() <= ringOffset.left + ringWidth &&
+                        plaviOffset.top + plavi.height() <= ringOffset.top + ringHeight;
+
+    var isCrveniInRing = crveniOffset.left >= ringOffset.left &&
+                         crveniOffset.top >= ringOffset.top &&
+                         crveniOffset.left + crveni.width() <= ringOffset.left + ringWidth &&
+                         crveniOffset.top + crveni.height() <= ringOffset.top + ringHeight;
 
     switch (e.keyCode) {
       case 39: // Strelica na desno
@@ -40,23 +58,36 @@ $(function() {
       case 40: // Strelica na dole
         $(plavi).css('top', currentUp + move + 'px'); // Povećaj 'top' za 10px
         break;
-      case 76:
-        crvenihp -= 1;
-        console.log(crvenihp);
-        $(".crveni p").text("HP:" + crvenihp);
+      case 76: // L
+        if (isPlaviInRing && isCrveniInRing) {
+          crvenihp -= 1;
+          console.log(crvenihp);
+          $(".crveni p").text("HP:" + crvenihp);
+        }
         break;
-      case 88:
-        plavihp -= 1;
-        console.log(plavihp);
-        $(".plavi p").text("HP:" + plavihp);
+      case 88: // X
+        if (isPlaviInRing && isCrveniInRing) {
+          plavihp -= 1;
+          console.log(plavihp);
+          $(".plavi p").text("HP:" + plavihp);
+        }
         break;
     }
 
-    if(currentRight < -1240){
-      window.location.href = "plavi.html";
+    // Provera da li je crveni izašao izvan granica ekrana
+    if (crveniOffset.left < 0 || crveniOffset.top < 0 || 
+        crveniOffset.left + crveni.width() > $(window).width() || 
+        crveniOffset.top + crveni.height() > $(window).height()) {
+      crvenihp = 0;
+      $(".crveni p").text("HP:" + crvenihp);
     }
-    else if(currentRightr < -1240){
-      window.location.href = "crveni.html";
+
+    // Provera da li je plavi izašao izvan granica ekrana
+    if (plaviOffset.left < 0 || plaviOffset.top < 0 || 
+        plaviOffset.left + plavi.width() > $(window).width() || 
+        plaviOffset.top + plavi.height() > $(window).height()) {
+      plavihp = 0;
+      $(".plavi p").text("HP:" + plavihp);
     }
   });
 });

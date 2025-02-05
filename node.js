dcee299aeed30e8fd279580c82887934978b06e5
sirac007006 <markonis007006@gmail.com>
2025-02-05 11:38:27 +0100

    Dodavanje izbora za lika i slusanje izbora

--- node.js
+++ node.js
@@ -12,6 +12,19 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
   res.render('index');
 });
+app.post('/player1', function(req, res) {
+  var choice1 = req.body.character;
+  console.log(choice1);
+  res.render('player2', {player1: choice1});
+});
+
+app.post('/player2', function(req, res) {
+  var choice2 = req.body.character;
+  var choice1 = req.body.player1; 
+  console.log(choice1);
+  console.log(choice2);
+  res.render('game', {player2: choice2, player1: choice1});
+});
 
 app.listen(port, function() {
   console.log("Server is running on port " + port);
